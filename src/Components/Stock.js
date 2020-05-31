c42d1981aa94071d1ca15c02b6db5ecf33fbee18
Luke <lmschless@gmail.com>
2020-05-31 10:47:32 -0700

    added useState to hold API response in component state. Mapping through items in response to component.

--- src/Components/Stock.js
+++ src/Components/Stock.js
@@ -1,28 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import axios from '../axios';
+import axios from 'axios';
 
 export default function Stocks() {
-	const getStockData = () => {
-		axios
-			.get(`/users`, {})
-			.then((res) => {
-				const data = res.data;
-				console.log(data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+	const [ data, setData ] = useState({ hits: [] });
+
+	useEffect(() => {
+		const getStockData = async () => {
+			const result = await axios(
+				'https://hn.algolia.com/api/v1/search?query=redux'
+			);
+			setData(result.data);
+		};
+		getStockData();
+	}, []);
+	console.log(data);
 
 	return (
-		<div>
-			<button
-				onClick={() => {
-					getStockData();
-				}}
-			>
-				test
-			</button>
-		</div>
+		<ul>
+			{data.hits.map((item) => (
+				<li key={item.objectID}>
+					<a href={item.url}>{item.title}</a>
+				</li>
+			))}
+		</ul>
 	);
 }

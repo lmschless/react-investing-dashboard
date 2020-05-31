23dfa94cc94f162ac09f46157544748581d7ed82
Luke <lmschless@gmail.com>
2020-05-31 11:29:14 -0700

    working api with Object.values and map to iterate through response and render to component.

--- src/Components/Stock.js
+++ src/Components/Stock.js
@@ -2,26 +2,30 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function Stocks() {
-	const [ data, setData ] = useState({ hits: [] });
+	const [ data, setData ] = useState({ metaData: [] });
 
 	useEffect(() => {
 		const getStockData = async () => {
 			const result = await axios(
-				'https://hn.algolia.com/api/v1/search?query=redux'
+				'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=12UGV4HUPE1MOT6Y'
+				// 'https://hn.algolia.com/api/v1/search?query=redux'
 			);
-			setData(result.data);
+			setData(result.data['Meta Data']);
 		};
 		getStockData();
 	}, []);
 	console.log(data);
 
+	// const symbol = data.["Meta Data"];
+	// console.log(symbol);
+
 	return (
 		<ul>
-			{data.hits.map((item) => (
-				<li key={item.objectID}>
-					<a href={item.url}>{item.title}</a>
-				</li>
-			))}
+			{Object.values(data).map((item) => {
+				return <li>{item}</li>;
+			})}
+
+			{/* {data.metaData.map((item) => <li>{item}</li>)} */}
 		</ul>
 	);
 }

f8b9192d262b18f23b2e62f416b1261d7a4f8d3f
Luke <lmschless@gmail.com>
2020-06-07 11:44:22 -0700

    working news API and mapping over response with Array.from .map

--- src/Components/NewsFeed.js
+++ src/Components/NewsFeed.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 export default function NewsFeed() {
+	const [ data, setData ] = useState({ test: [] });
+
+	useEffect(() => {
+		const getNews = async () => {
+			const result = await axios(
+				'http://newsapi.org/v2/everything?q="stock market"&sortBy=publishedAt&apiKey=90ddee78a57f435fa9efe02754a6176a'
+			);
+			console.log(result.data.articles);
+			setData(result.data.articles);
+		};
+		getNews();
+	}, []);
+	console.log(data);
+
 	const Wrapper = styled.div`
-		min-width: 18vw;
+		min-width: 25vw;
 		max-width: 23vw;
 		border: 2px solid black;
 		float: right;
+		padding: .5em;
 	`;
 
 	const News = styled.div`margin-top: 100px;`;
 
 	return (
-		<Wrapper>
-			<News>
-				<ul>
-					<li>test</li>
-				</ul>
-			</News>
-		</Wrapper>
+		<React.Fragment>
+			<Wrapper>
+				<News>
+					{Array.from(data).map((item) => <li key={item.id}>{item.title}</li>)}
+				</News>
+			</Wrapper>
+		</React.Fragment>
 	);
 }

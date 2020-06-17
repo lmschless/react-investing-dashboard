9777d21902ba9f7ba6a1f4d9e2b1c7716f222a6e
Luke <lmschless@gmail.com>
2020-06-16 19:41:13 -0700

    last push before refactoring from hooks to class state. Should simplify the project for an MVP.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';

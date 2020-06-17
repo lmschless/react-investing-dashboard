25ad83917dfd90c2006b0bec726d6a4d0dacc004
Luke <lmschless@gmail.com>
2020-06-16 20:52:24 -0700

    have working class local state.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
@@ -9,7 +9,7 @@ import Footer from './Footer';
 import NewsCard from './NewsCard';
 import axios from 'axios';
 
-const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
 	root: {
 		display: 'flex'
 	},
@@ -28,95 +28,107 @@ const useStyles = makeStyles((theme) => ({
 		flexGrow: 1,
 		padding: theme.spacing(3)
 	}
-}));
+});
 
-export default function Main() {
-	const [ data, setData ] = useState({ metaData: [] });
-	const [ symbols, setSymbol ] = useState({
-		default: [ 'IBM', 'MSFT', 'AAPL' ]
-	});
+export class Main extends Component {
+	constructor(props) {
+		super(props);
 
-	useEffect(() => {
-		const getStockData = async () => {
-			// hard coded
-			// const result = {
-			// 	'Meta Data': {
-			// 		'1. Information':
-			// 			'Weekly Prices (open, high, low, close) and Volumes',
-			// 		'2. Symbol': 'IBM',
-			// 		'3. Last Refreshed': '2020-05-29',
-			// 		'4. Time Zone': 'US/Eastern'
-			// 	}
-			// };
-			//////////////////////
-			// LIVE API:
-			const result = await axios(
-				'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=12UGV4HUPE1MOT6Y'
-			);
-			///// hard coded:
-			// setData(result['Meta Data']);
-			//  LIVE API:
-			setData(result.data['Meta Data']);
+		this.state = {
+			stocks: [],
+			image: require('./../assets/stock-placeholder.jpg')
 		};
-		getStockData();
-	}, []);
-	console.log('Main data:');
-	console.log({ data });
-	console.log({ symbols });
+	}
+
+	componentDidMount = async () => {
+		const result = await axios(
+			'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=12UGV4HUPE1MOT6Y'
+		);
+		console.log(result);
+		const data = result.data['Meta Data'];
+		this.setState({ stocks: data });
+		console.log(this.state.stocks);
+	};
+	// const [ data, setData ] = useState({ metaData: [] });
+	// const [ symbols, setSymbol ] = useState({
+	// 	default: [ 'IBM', 'MSFT', 'AAPL' ]
+	// });
+
+	// useEffect(() => {
+	// 	const getStockData = async () => {
+	// hard coded
+	// const result = {
+	// 	'Meta Data': {
+	// 		'1. Information':
+	// 			'Weekly Prices (open, high, low, close) and Volumes',
+	// 		'2. Symbol': 'IBM',
+	// 		'3. Last Refreshed': '2020-05-29',
+	// 		'4. Time Zone': 'US/Eastern'
+	// 	}
+	// };
+	//////////////////////
+	// LIVE API:
+	// const result = await axios(
+	// 	'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=12UGV4HUPE1MOT6Y'
+	// );
+	///// hard coded:
+	// setData(result['Meta Data']);
+	//  LIVE API:
+	// 		setData(result.data['Meta Data']);
+	// 	};
+	// 	getStockData();
+	// }, []);
+	// console.log('Main data:');
+	// console.log({ data });
+	// console.log({ symbols });
 	// const symbol = data.["Meta Data"];
 	// console.log(symbol);
-	const image = require('./../assets/stock-placeholder.jpg');
 
-	const classes = useStyles();
+	render() {
+		const { classes } = this.props;
+		const gridContainer = {
+			display: 'grid',
+			gridTemplateColumns: 'repeat(auto-fill, minmax(18em, 5fr))',
+			gridTemplateRows: 'repeat(auto-fill, .5fr)',
+			gridColumnGap: '.75em',
+			gridRowGap: '1em',
+			minHeight: '80vh'
+			// padding: '2em'
+			// margin: '5em'
+		};
 
-	const gridContainer = {
-		display: 'grid',
-		gridTemplateColumns: 'repeat(auto-fill, minmax(18em, 5fr))',
-		gridTemplateRows: 'repeat(auto-fill, .5fr)',
-		gridColumnGap: '.75em',
-		gridRowGap: '1em',
-		minHeight: '80vh'
-		// padding: '2em'
-		// margin: '5em'
-	};
-
-	return (
-		<React.Fragment>
-			<div className={classes.root}>
-				{/* <AppBar position="fixed">
+		return (
+			<React.Fragment>
+				<div className={classes.root}>
+					{/* <AppBar position="fixed">
 				<Toolbar>
 					<Typography variant="h6" noWrap>
 						Luke's Investing Portfolio
 					</Typography>
 				</Toolbar>
 			</AppBar> */}
-				<main className={classes.content}>
-					{/* <div className={classes.toolbar} /> */}
-					{/* <Typography paragraph> */}
+					<main className={classes.content}>
+						{/* <div className={classes.toolbar} /> */}
+						{/* <Typography paragraph> */}
 
-					{/* <StockList /> */}
-					<div style={gridContainer}>
-						{/* {Object.values(data).map((stock) => {
+						{/* <StockList /> */}
+						<div style={gridContainer}>
+							{/* <Stock name= */}
+							{/* {Object.values(data).map((stock) => {
 							return <Stock name={stock.symbol} image={stock.urlToImage} />;
 						})} */}
 
-						{Array.from(data).map((stock) => (
-							<Stock key={stock.symbol} name={stock.symbol} img={image} />
-						))}
-
-						{/* <Stock />
-						<Stock />
-						<Stock />
-						<Stock />
-						<Stock />
-						<Stock />
-						<Stock /> */}
-					</div>
-					{/* </Typography> */}
-				</main>
-				{/* <NewsCard /> */}
-			</div>
-			<Footer />
-		</React.Fragment>
-	);
+							{/* {Array.from(data).map((stock) => ( */}
+							{/* // 	<Stock key={stock.symbol} name={stock.symbol} img={image} />
+						// ))} */}
+						</div>
+						{/* </Typography> */}
+					</main>
+					{/* <NewsCard /> */}
+				</div>
+				<Footer />
+			</React.Fragment>
+		);
+	}
 }
+export default withStyles(styles, { withTheme: true })(Main);
